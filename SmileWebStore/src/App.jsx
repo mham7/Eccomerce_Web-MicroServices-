461e09755f212ddc8b89a893da4cb93f849d0cc6
mham7 <m.hammadcr7777@gmail.com>
2024-03-11 21:57:46 +0500

    First commit

---
+++ SmileWebStore/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from '../src/pages/login.jsx'
+
+function App() {
+
+  return (
+   <Login></Login>
+    
+  )
+}
+
+export default App
---
+++ SmileWebStore/src/pages/login.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../styles/login.css';
+import Logo from '../assets/Logo/Logo.png'
+const Login = () => {
+  return (
+    <div className='form-container'>
+    <div className='logo'>
+      <img src={Logo} alt="Logo"/>
+    </div>
+    <input
+          className="textfield"
+          placeholder="Email"
+        />
+    <input
+          className="textfield"
+          placeholder="Password"
+    />
+    <button className='button'>Login</button>
+    <div className='note'>Not a member?Signup</div>
+    </div>
+    
+  )
+}
+
+export default Login
